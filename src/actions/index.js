26dde56c66f9872c2625176285f82b013cd5059d
Dani Renner <danijrenner@gmail.com>
2022-01-10 14:58:08 -0800

    add store and custom middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,19 @@
 import * as c from './ActionTypes';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { createStore } from 'redux';
+import reducer from './reducers/headlines-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 export const requestHeadlines = () => ({
   type: c.REQUEST_HEADLINES
