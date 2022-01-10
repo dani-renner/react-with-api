02925f5411cff4c9b3b5b1216963362689bd92ce
Dani Renner <danijrenner@gmail.com>
2022-01-10 14:01:21 -0800

    add get headline failure case to reducer, test pass

--- src/__test__/reducers/headlines-reducer.test.js
+++ src/__test__/reducers/headlines-reducer.test.js
@@ -51,10 +51,17 @@ describe('headlinesReducer', () => {
     });
   });
 
-  // test('successfully getting headlines should change isLoading to false and update headlines', () => {
-  //   const headlines = "A headline";
-  //   action = {
-  //     type: c.GET_HEADLINES_SUCCESS
-  //   }
-  // })
+  test('failing to get headlines should change isLoading to false and add an error message', () => {
+    const error = "An error";
+    action = {
+      type: c.GET_HEADLINES_FAILURE,
+      error
+    };
+
+    expect(headlinesReducer(loadingState, action)).toEqual({
+      isLoading: false,
+      headlines: [],
+      error: "An error"
+    });
+  });
 });
