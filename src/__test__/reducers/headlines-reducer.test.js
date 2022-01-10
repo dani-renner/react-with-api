fb9d3ca89b5cdd37ec5400693aa4520bc873ab13
Dani Renner <danijrenner@gmail.com>
2022-01-10 13:49:19 -0800

    add ability to update headlines, test pass

--- src/__test__/reducers/headlines-reducer.test.js
+++ src/__test__/reducers/headlines-reducer.test.js
@@ -9,6 +9,11 @@ describe('headlinesReducer', () => {
     headlines: [],
     error: null,
   };
+  const loadingState = {
+    isLoading: true,
+    headlines: [],
+    error: null
+  }
 
   test('should successfully return the default state if no action is passed into it', () => {
     expect(headlinesReducer(defaultState, {type: null})).toEqual(
@@ -31,4 +36,25 @@ describe('headlinesReducer', () => {
       error: null
     });
   });
+
+  test('successfully getting headlines should change isLoading to false and update headlines', () => {
+    const headlines = "A headline";
+    action = {
+      type: c.GET_HEADLINES_SUCCESS,
+      headlines
+    };
+
+    expect(headlinesReducer(loadingState, action)).toEqual({
+      isLoading: false,
+      headlines: "A headline",
+      error: null
+    });
+  });
+
+  // test('successfully getting headlines should change isLoading to false and update headlines', () => {
+  //   const headlines = "A headline";
+  //   action = {
+  //     type: c.GET_HEADLINES_SUCCESS
+  //   }
+  // })
 });
