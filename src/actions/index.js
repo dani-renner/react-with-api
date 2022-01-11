95f9f164611a9e8587d404240595549cceb11f99
Dani Renner <danijrenner@gmail.com>
2022-01-10 18:00:34 -0800

    move API call to index.js, use thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,13 @@ import * as c from './ActionTypes';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import middlewareLogger from '../middleware/middleware-logger';
 import reducer from './reducers/headlines-reducer';
 import { Provider } from 'react-redux';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, middlewareLogger));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -28,3 +30,18 @@ export const getHeadlinesFailure = (error) => ({
   type: c.GET_HEADLINES_FAILURE,
   error
 });
+
+export const makeApiCall = () => {
+  return dispatch => {
+    dispatch(requestHeadlines);
+    return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+      .then(response => response.json())
+      .then(
+        (jsonifiedResponse) => {
+          dispatch(getHeadlinesSuccess(jsonifiedResponse.results));
+        })
+      .catch((error) => {
+        dispatch(getHeadlinesFailure(error));
+      });
+  }
+}
